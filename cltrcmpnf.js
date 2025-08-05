8a2e512f483f94c63b0750f014680927ff33d448
EjkxElectronick <jekerx@gmail.com>
2025-08-05 16:22:58 -0400

    Create cltrcmpnf.js

---
+++ cltrcmpnf.js
@@ -0,0 +1,17 @@
+<script>
+jQuery(function () {
+  const campo = jQuery("#helpdesk_ticket_custom_field_cf_info_1588414");
+
+  // Ocultar el campo
+  campo.hide();
+
+  // Ocultar el label asociado (si está antes o relacionado por for/id)
+  const label = jQuery(`label[for="helpdesk_ticket_custom_field_cf_info_1588414"]`);
+  if (label.length) {
+    label.hide();
+  } else {
+    // Si el label no tiene 'for', tratar de ocultar el anterior
+    campo.prev("label").hide();
+  }
+});
+</script>
